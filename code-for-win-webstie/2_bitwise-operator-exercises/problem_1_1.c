6d9d30ecb62d51328e765e02ac73c744bd8c881c
Syed Al Mohaimin <120173044+AlMohaimin037@users.noreply.github.com>
2023-02-21 20:36:02 +0600

    2nd problem done

--- code-for-win-webstie/2_bitwise-operator-exercises/problem_1_1.c
+++ code-for-win-webstie/2_bitwise-operator-exercises/problem_1_1.c
@@ -16,7 +16,7 @@ int main()
     if(num & 1)
         printf("LSB of %d is set (1).", num);
     else
-        printf("LSB of %d is set (0).", num);
-        
+        printf("LSB of %d is unset (0).", num);
+
     return 0;
 }
